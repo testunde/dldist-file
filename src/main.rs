5de295e297a442aabe9e4688dc2f7d4028142e61
Tobias Neumann <tobias.neumann@uni-wuerzburg.de>
2024-12-07 16:06:06 +0100

    Continue skeleton implementation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@ use std::io::{self, BufRead};
 use std::path::{Path, PathBuf};
 
 use clap::Parser;
+use num_traits::PrimInt;
+
+struct DistanceResult {
+    line_a: u32,
+    line_b: u32,
+    dldist: u32,
+}
+
+const NUM_PRINT_ALL: u16 = 0;
 
 /// Returns an Iterator to the Reader of the lines of the file.
 /// Preserves order and count of the raw file lines.
@@ -20,9 +29,50 @@ where
     Ok(lines_filtered)
 }
 
+// /// Returns the amount of the given number. Requries O(N) memory.
+// fn factorial(num: u128) -> u128 {
+//     if num < 2 {
+//         1
+//     } else {
+//         (2..=num).product()
+//     }
+// }
+
+// /// Returns the amount of pair-combinations using factorial calculation
+// fn pair_combinations_count_factorial(num: u64) -> u64 {
+//     if num < 2 {
+//         0
+//     } else {
+//         (factorial(num as u128) / (2u128 * factorial(num as u128 - 2u128))) as u64
+//     }
+// }
+
+/// Returns the amount of pair-combinations
+fn pair_combinations_count<T>(num: T) -> T
+where
+    T: PrimInt + std::convert::From<u32>,
+{
+    if num < 2u32.into() {
+        0u32.into()
+    } else {
+        (num * (num - 1u32.into())) / 2u32.into()
+    }
+}
+
+fn calculate_dl_distances(lines: &Vec<String>) -> Vec<DistanceResult> {
+    let lines_cnt = lines.len();
+    let combinations_cnt = pair_combinations_count(lines_cnt as u64);
+    let results = Vec::with_capacity(combinations_cnt as usize);
+
+    // TODO: implement calculate_dl_distances()
+
+    results
+}
+
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Arguments {
+    // TODO: also accept conent from stdin ('-')
     /// Input raw text file to analyse.
     #[clap(required = true)]
     input_file: PathBuf,
@@ -51,23 +101,61 @@ fn main() {
             None => panic!("Failed to build string from PathBuf (input file)!"),
         }
     );
-    let _lines = match read_lines(args.input_file) {
+    let lines = match read_lines(args.input_file) {
         Ok(lns) => lns,
         Err(error) => panic!("Failed to read in lines from file: {error:?}"),
     };
+    let lines_cnt = lines.len();
+    if lines_cnt < 2 {
+        println!(
+            "The file has to contain at least two lines! Counted {}.",
+            lines_cnt
+        );
+        return;
+    }
 
-    println!("==> Calculating Damerau-Levenshtein distances...");
-    // TODO: also print combinatorial amount of distances to calculate
-    // TODO: calculate
-
+    let combinations_cnt = pair_combinations_count(lines_cnt as u32);
     println!(
-        "==> Printing {} results:",
-        if args.n_pairs == 0 {
-            String::from("all")
+        "==> Calculating {} Damerau-Levenshtein distances between {} lines...",
+        combinations_cnt, lines_cnt
+    );
+    // calculate all distances
+    let mut distance_results = calculate_dl_distances(&lines);
+    if distance_results.len() as u32 != combinations_cnt {
+        panic!("Somehow the size of the result combinations list does not equal the theoretical count!?");
+    }
+    // sort depending on user settings
+    if args.descending {
+        distance_results.sort_by(|a, b| b.dldist.cmp(&a.dldist));
+    } else {
+        distance_results.sort_by(|a, b| a.dldist.cmp(&b.dldist));
+    }
+
+    let print_cnt_limit = combinations_cnt.min(args.n_pairs as u32);
+    println!(
+        "==> Printing {} results in {} order:",
+        if args.n_pairs == NUM_PRINT_ALL {
+            format!("all {}", combinations_cnt)
         } else {
-            format!("top {}", args.n_pairs)
+            format!("top {}", print_cnt_limit)
+        },
+        if args.descending {
+            "descending"
+        } else {
+            "ascending"
         }
     );
-    // TODO: sort
-    // TODO: print
+    let print_cnt = if args.n_pairs == NUM_PRINT_ALL {
+        combinations_cnt
+    } else {
+        print_cnt_limit
+    };
+    for i in 0..print_cnt as usize {
+        let dr = &distance_results[i];
+        // print padded values
+        println!(
+            "Line {:.>4} vs. {:.>4}: {:.>4}",
+            dr.line_a, dr.line_b, dr.dldist
+        );
+    }
 }
